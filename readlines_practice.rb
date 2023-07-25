817e271b7234751c4a613383b46ab41eec113949
EC2 Default User <129802934+Mika-Tachikawa@users.noreply.github.com>
2023-07-25 11:34:46 +0000

    Initial Commit1

---
+++ readlines_practice.rb
@@ -0,0 +1,11 @@
+lines = readlines
+lines.each do |words|
+  p words.chomp.split(' ')
+end
+
+#lines = readlines
+  #lines.each do |words|
+    #words.chomp.split(' ').each do |text|
+     #p text
+    #end
+  #end
